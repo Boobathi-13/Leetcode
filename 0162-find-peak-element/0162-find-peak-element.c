d34df7987a9bc2f5dd55eb324645e8dea78859c1
Boobathi <boobathirajakm.22ece@kongu.edu>
2024-12-28 16:17:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.08%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.c
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -1,16 +1,17 @@
-int findPeakElement(int* a, int n) {
-    for(int i=0; i<n-1; i++){
-        if(a[i]>a[i+1])
-            return i;
-    }
-    return n-1;
-}
-
 // int findPeakElement(int* a, int n) {
-//     int left=0, right = n-1,mid;
-//     while(left<=right){
-//         mid=(left+right)/2;
-//         if(a[mid]>a[mid+1] && a[mid]>a[mid-1]) return mid;
-//         else if(a[mid])
+//     for(int i=0; i<n-1; i++){
+//         if(a[i]>a[i+1])
+//             return i;
 //     }
+//     return n-1;
 // }
+
+int findPeakElement(int* a, int n) {
+    int left=0, right = n-1,mid;
+    while(left<right){
+        mid=(left+right)/2;
+        if(a[mid]>a[mid+1]) right=mid;
+        else left=mid+1;
+    }
+    return right;
+}
