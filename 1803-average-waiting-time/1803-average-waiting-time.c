f3c4addba079c0ed52acaafedd8d830b394d862a
Boobathi <boobathirajakm.22ece@kongu.edu>
2025-01-02 12:06:58 +0530

    Time: 0 ms (100%), Space: 29.9 MB (36%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.c
+++ 1803-average-waiting-time/1803-average-waiting-time.c
@@ -1,14 +1,24 @@
 double averageWaitingTime(int** a, long n, int* m) {
-    double sum = 0, sum1 = 0;
-    sum = a[0][0] + a[0][1];
-    for(int i = 1; i<n; i++){
-        for(int j=1; j<*m; j++){
-            if(a[i][0]>=sum) sum = a[i][0] + a[i][j];
-            else sum += a[i][j];
-            printf("SUM : %d\n",sum);
+    double totalWaitingTime = 0;
+    int currentTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        int arrivalTime = a[i][0];
+        int prepTime = a[i][1];
+
+        // If the chef is free before the customer arrives, he starts at the customer's arrival time
+        if (arrivalTime >= currentTime) {
+            currentTime = arrivalTime + prepTime;
+        } else {
+            // Otherwise, the chef starts preparing the order as soon as he finishes the previous one
+            currentTime += prepTime;
         }
-        sum1 += sum - a[i][0];
-        printf("%d\n",sum1);
+
+        // Calculate the waiting time for this customer
+        int waitingTime = currentTime - arrivalTime;
+        totalWaitingTime += waitingTime;
     }
-    return (double)(a[0][1]+sum1)/n;
+
+    // Return the average waiting time
+    return totalWaitingTime / n;
 }
