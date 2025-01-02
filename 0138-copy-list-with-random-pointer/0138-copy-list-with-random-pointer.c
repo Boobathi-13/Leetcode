8937bdba6d751eb1a327bd43cfc85d5ed9c460d7
Boobathi <boobathirajakm.22ece@kongu.edu>
2025-01-02 11:02:57 +0530

    Time: 8 ms (69.77%), Space: 14.2 MB (71.32%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.c
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.c
@@ -7,24 +7,44 @@
  * };
  */
 
-struct Node* copyRandomList(struct Node *head) {
-    if(head == NULL) return NULL;
-    struct Node *newHead = (struct Node *)malloc(sizeof(struct Node));
-    newHead->val = head->val; newHead->next = NULL;
-    newHead->random = head->random;
-    head->random = newHead;
-    struct Node *n1 = newHead, *new;
-    for(struct Node *n2 = head->next; n2; n2 = n2->next){
-        new = (struct Node *)malloc(sizeof(struct Node));
-        new->val = n2->val;
-        new->random = n2->random;
-        n2->random = new;
-        n1->next = new;
-        n1 = new;
+struct Node* copyRandomList(struct Node* head) {
+
+    struct Node* orig = head;
+
+    if (!orig)
+        return orig;
+    //struct Node* new = NULL;
+    while ( orig  != NULL ){
+        struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+        new->val = orig->val;
+        new->next = orig->next;
+        new->random = NULL;
+        orig->next = new;
+        orig = new->next;
     }
-    n1->next=NULL;
-    for(n1 = newHead;n1; n1=n1->next)
-        if(n1->random)
-            n1->random = n1->random->random;
+
+    orig = head;
+    
+
+    while ( orig != NULL ){
+        if ( orig->random ){
+            orig->next->random = orig->random->next;
+        }
+        orig = orig->next->next;        
+    }
+
+    orig = head;
+    struct Node* newHead = orig->next;
+    struct Node* l1 = orig , *l2 = orig->next;
+    while (l1 && l2){
+        l1->next = l1->next->next;
+        if (l2->next)
+            l2->next = l2->next->next;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+
     return newHead;
+
+    
 }
