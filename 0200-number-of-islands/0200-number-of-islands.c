9a859b104da5ce3bec358a5a13eed8ade44e590f
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-01-06 16:20:49 +0530

    Time: 19 ms (84.69%), Space: 11.7 MB (57.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.c
+++ 0200-number-of-islands/0200-number-of-islands.c
@@ -1,37 +1,31 @@
-void help(char** grid, int row, int col, int x, int y) {
+#include <stdbool.h>
 
-    grid[x][y] = '0';
+void dfs(char** grid, int i, int j, int m, int n) {
+    // Base case: Out of bounds or water ('0')
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
 
-    if (x - 1 >= 0 && grid[x - 1][y] == '1')
-        help(grid, row, col, x - 1, y);
-    if (x + 1 < row && grid[x + 1][y] == '1')
-        help(grid, row, col, x + 1, y);
-    if (y - 1 >= 0 && grid[x][y - 1] == '1')
-        help(grid, row, col, x, y - 1);
-    if (y + 1 < col && grid[x][y + 1] == '1')
-        help(grid, row, col, x, y + 1);
+    // Mark the current cell as visited by setting it to '0'
+    grid[i][j] = '0';
+
+    // Explore all 4 directions
+    dfs(grid, i - 1, j, m, n); // Up
+    dfs(grid, i + 1, j, m, n); // Down
+    dfs(grid, i, j - 1, m, n); // Left
+    dfs(grid, i, j + 1, m, n); // Right
 }
 
 int numIslands(char** grid, int gridSize, int* gridColSize) {
+    int m = gridSize;          // Number of rows
+    int n = *gridColSize;      // Number of columns
+    int count = 0;             // Island count
 
-    int row, col;
-
-    row = gridSize;
-    col = *gridColSize;
-
-    int ans = 0;
-
-    for (int i = 0; i < row; i++) {
-
-        for (int j = 0; j < col; j++) {
-
-            if (grid[i][j] == '1') {
-                help(grid, row, col, i, j);
-                ans++;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == '1') { // Found an unvisited land cell
+                dfs(grid, i, j, m, n); // Explore the entire island
+                count++; // Increment the island count
             }
         }
     }
-
-    return ans;
-
+    return count;
 }
