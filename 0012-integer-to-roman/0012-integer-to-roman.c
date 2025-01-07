9e9be22c5fce6ab8622c954d23b65f6ecfadc62b
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-01-07 19:49:20 +0530

    Time: 0 ms (100%), Space: 9.1 MB (38.92%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -0,0 +1,21 @@
+char* intToRoman(int num) 
+{
+    int n[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char* s[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"}; //To store strings like "CM" and "CD", you need an array of pointers to strings (char* s[13]).
+    
+    int i=0;
+    static char ch[20]="\0";
+    ch[0] = '\0';
+    while(num>0)
+    {
+        if(num>=n[i])
+        {
+            strcat(ch, s[i]);
+            num-=n[i];
+        }else
+        {
+            i++;
+        }
+    }
+    return ch;
+}
