00f08282e2a8bcde444644250f7b1472d81ea444
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-01-19 12:06:51 +0530

    Time: 11 ms (8.32%), Space: 15.8 MB (33.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
+        // Base case: both open and close are used up
+        if (open == 0 && close == 0) {
             ans.push_back(op);
+            cout << "Adding to result: " << op << endl;
             return;
         }
-        //when count of open and close brackets are same then 
-        //we have only one choice to put open bracket 
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+        // When open and close are the same, we can only add '('
+        if (open == close) {
+            solve(op + '(', open - 1, close, ans);
         }
-        else if(open == 0){
-            //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
+        // When no open parentheses are left, we can only add ')'
+        else if (open == 0) {
+            solve(op + ')', open, close - 1, ans);
         }
-        else if(close == 0){
-            //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+        // When no close parentheses are left, we can only add '('
+        else if (close == 0) {
+            solve(op + '(', open - 1, close, ans);
         }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
+        // Otherwise, we have a choice to add '(' or ')'
+        else {
+            solve(op + '(', open - 1, close, ans);
+            solve(op + ')', open, close - 1, ans);
         }
     }
     vector<string> generateParenthesis(int n) {
