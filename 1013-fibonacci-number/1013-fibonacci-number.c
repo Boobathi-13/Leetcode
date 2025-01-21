11660f71b47ebcdc81768d923b16c1afd6e361da
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-01-21 11:43:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (47.15%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -0,0 +1,13 @@
+int fib1(int n, int dp[]) {
+    if (n <= 1) return n; // Base cases: fib(0) = 0, fib(1) = 1
+    if (dp[n] != -1) return dp[n]; // Use precomputed result if available
+    dp[n] = fib1(n-1,dp) + fib1(n-2,dp); // Recursively compute and store result
+    return dp[n];
+}
+
+int fib(int n) {
+    int dp[31];
+    memset(dp, -1, sizeof(dp)); // Initialize all values to -1
+    // int dp[31] = {-1}; // Initialize all values to -1
+    return fib1(n,dp);
+}
