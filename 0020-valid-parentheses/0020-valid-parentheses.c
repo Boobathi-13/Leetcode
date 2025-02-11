92c023def97b25d26c55d1b7aa94531917f419f9
Boobathi Raja K M <boobathirajakm.22ece@kongu.edu>
2025-02-11 23:33:51 +0530

    Time: 3 ms (8.55%), Space: 7.9 MB (61.88%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,14 +1,13 @@
 bool isValid(char* s) {
-    int n = strlen(s);
     char stack[10000];
     int top = -1;
-    for(int i=0; i<n; i++){
-        if(s[i]=='('||s[i]=='{'||s[i]=='[') stack[++top]=s[i];
-        else{
-            if(top>=0 && (stack[top]=='('&&s[i]==')' || stack[top]=='{'&&s[i]=='}' || stack[top]=='['&&s[i]==']')) top--;
-            else return false;
-        }
+    for(int i=0; i<strlen(s); i++) {
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[') s[++top]=s[i];
+        else if(top!= -1 && s[i]==')' && s[top]=='(') top--;
+        else if(top!= -1 && s[i]=='}' && s[top]=='{') top--;
+        else if(top!= -1 && s[i]==']' && s[top]=='[') top--;
+        else return false;
     }
-    if(top == -1) return true;
+    if(top==-1) return true;
     else return false;
 }
